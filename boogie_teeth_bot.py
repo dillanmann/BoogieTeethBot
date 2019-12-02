2822745c84b228032380a27d3e4e9e4146c8bfd1
Dillanm <dillan.mann@gmail.com>
2019-12-02 19:18:59 +0000

    initial commit

---
+++ boogie_teeth_bot.py
@@ -0,0 +1,90 @@
+from io import open
+from flask import Flask
+from telegram import InlineQueryResultArticle, ParseMode, InputTextMessageContent
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+from telegram.utils.helpers import escape_markdown
+from uuid import uuid4
+import os
+import logging
+import random
+
+app = Flask(__name__)
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
+images = []
+
+def teeth(update, context):
+    """
+    Callback for when the /teeth command is used
+    Selects a random teeth image and replies
+    """
+    global images
+    index = random.randint(0, len(images)-1)
+    image = images[index]
+    with open(image, 'rb') as img:
+        update.message.reply_photo(photo=img)
+
+def inlinequery(update, context):
+    """Handle the inline query."""
+    query = update.inline_query.query
+    results = [
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Caps",
+            input_message_content=InputTextMessageContent(
+                query.upper())),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Bold",
+            input_message_content=InputTextMessageContent(
+                "*{}*".format(escape_markdown(query)),
+                parse_mode=ParseMode.MARKDOWN)),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Italic",
+            input_message_content=InputTextMessageContent(
+                "_{}_".format(escape_markdown(query)),
+                parse_mode=ParseMode.MARKDOWN))]
+
+    update.inline_query.answer(results)
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def start_bot():
+    token = os.environ["TELEGRAM_API_TOKEN"]
+    updater = Updater(token, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("teeth", teeth))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(InlineQueryHandler(inlinequery))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    logger.info("Bot starting...")
+    updater.start_polling()
+
+    logger.info("Bot started successfully")
+    updater.idle()
+
+def index_images():
+    global images
+    root = os.path.dirname(__file__)
+    directory = os.path.join(root, "images")
+    images = [os.path.join(directory, f) for f in os.listdir(directory)]
+    logger.debug("Found {} image files".format(len(images)))
+
+def main():
+    index_images()
+    start_bot()
+
+if __name__ ==  "__main__":
+    main()
